c795d1f4e766d5ee1c70e76bea033bac3b2419ae
nhi-ngo <nhingo.y@gmail.com>
2025-06-06 10:21:00 -0500

    Initial Commit

---
+++ Letterlogue/LetterlogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterlogueApp.swift
+//  Letterlogue
+//
+//  Created by Nhi Ngo on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LetterlogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
